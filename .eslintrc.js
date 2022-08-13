b4b82619ed05075561c0d87c250552d158a2a518
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2022-08-13 15:48:40 +0300

    added rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,19 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
-  rules: {},
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
 };
