3de5764e6e30aebbc86f7ec5ad189af8a7de6d36
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2022-09-10 19:00:27 +0300

    getRoadmap resolvers added, createRoadmap mutation fix

--- src/resolvers/roadmapResolver.ts
+++ src/resolvers/roadmapResolver.ts
@@ -5,6 +5,37 @@ const roadmapResolvers = {
     getRoadmap: async (_, { _id }) => {
       return await Roadmap.findById({ _id });
     },
+    getRoadmaps: async (_, args) => {
+      const { search = null, page = 1, limit = 20 } = args;
+      let searchQuery = {};
+      if (search) {
+        //update search query
+        searchQuery = {
+          $or: [
+            {
+              title: { $regex: search, $options: "i" },
+            },
+            {
+              tags: { $regex: search, $options: "i" },
+            },
+          ],
+        };
+      }
+
+      const roadmaps = await Roadmap.find(searchQuery)
+        .limit(limit)
+        .skip((page - 1) * limit)
+        .lean();
+      const count = await Roadmap.countDocuments(searchQuery);
+      return {
+        roadmaps,
+        totalPages: Math.ceil(count / limit),
+        currentPage: page,
+      };
+    },
+    getRoadmapsOfUser: async (_, { userId }) => {
+      return await Roadmap.find({ userId });
+    },
   },
   Mutation: {
     createRoadmap: async (
--- src/typeDefs/roadmapTypeDefs.ts
+++ src/typeDefs/roadmapTypeDefs.ts
@@ -9,16 +9,23 @@ const roadmapTypeDefs = gql`
     elements: String
     userId: String
   }
+  type RoadmapResult {
+    roadmaps: [Roadmap]
+    currentPage: Int
+    totalPages: Int
+  }
   type Query {
     getRoadmap(_id: ID!): Roadmap
+    getRoadmaps(search: String, page: Int, limit: Int): RoadmapResult
+    getRoadmapsOfUser(userId: ID!): [Roadmap]
   }
   type Mutation {
     createRoadmap(
-      title: String
-      description: String
-      tags: [String]
-      elements: String
-      userId: String
+      title: String!
+      description: String!
+      tags: [String!]
+      elements: String!
+      userId: String!
     ): Roadmap
   }
 `;
